6be44b41b0c0f47f09008d58cf48150b28e6f376
Flench04 <47568518+Fleench@users.noreply.github.com>
2025-07-10 13:28:59 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-: "${REPO_URL:=https://raw.githubusercontent.com/Fleench/Contact-Link/main}"
+: "${REPO_URL:=https://raw.githubusercontent.com/Fleench/Contact-Link/refs/heads/master/}"
 
 FILES="main.ts styles.css manifest.json versions.json"
 
